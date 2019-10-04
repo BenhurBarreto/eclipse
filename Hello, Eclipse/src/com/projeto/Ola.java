aab78fbceadc3e5bbfee7714fe6d5c0fba946a3a
Benhur-Linux <benhurbbs@gmail.com>
2019-10-04 08:32:00 -0300

    Adicionando Hello

---
+++ Hello, Eclipse/src/com/projeto/Ola.java
@@ -0,0 +1,10 @@
+package com.projeto;
+
+public class Ola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo, essa é a minha primeira app em Java.");
+	}
+
+}
