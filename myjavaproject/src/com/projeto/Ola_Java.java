bac0e2e1d0501aa3059121974fe1c763462985d3
Benhur-Linux <benhurbbs@gmail.com>
2019-10-06 16:40:12 -0300

    Primeiro Commit do Projeto em Java

---
+++ myjavaproject/src/com/projeto/Ola_Java.java
@@ -0,0 +1,9 @@
+package com.projeto;
+
+public class Ola_Java {
+
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo do Java");
+	}
+
+}
---
+++ myjavaproject/src/module-info.java
@@ -0,0 +1,2 @@
+module google.myjavaproject {
+}
